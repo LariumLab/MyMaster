a9637de567411760092209724c049330eaf7378a
Vetas <ram010172@yandex.ru>
2018-04-27 08:49:49 +0300

    Blur loader added as extension of UIView

--- Scheduling/AuthGroup/InitialViewController.swift
+++ Scheduling/AuthGroup/InitialViewController.swift
@@ -8,51 +8,64 @@
 
 import UIKit
 
+
 class InitialViewController: UIViewController {
 	
 	var token: SecKey!
     var TabBar: UIViewController = UIViewController()
-    
+	let serverAdr = "http://localhost:8080/"
+
+	
+	
+	
     override func viewDidLoad() {
         super.viewDidLoad()
-		
-		let storyboard = UIStoryboard(name: "Main", bundle: nil)
-		let TabBarC = storyboard.instantiateViewController(withIdentifier: "TabBar") as! UITabBarController
-		
-		// Adding user's token to keychain (if needed)
-		/*
-		let key: String = "userToken"
-		let data = "testtoken".data(using: .utf8)!
-		
-		if Keychain.save(key: key, data: data)  == errSecSuccess {
-			print("tokenAdded")
-		}
-		*/
-		// Now we are checking, if there is some user's token in keychain
-		// if true, we can determine the type of account and then we can present appropriate tabbar
-		let token = Keychain.load(key: "userToken")
-		if token != nil {
-			
-			let ClientProfileNavigationC = storyboard.instantiateViewController(withIdentifier: "ClientNavigationController")
-			TabBarC.viewControllers?.append(ClientProfileNavigationC)
-			let user = Account(login: "client", password: "client", profileType: .client)
-			loadData(acc: user)
-			
-		} else { // else we can present the registration view (now it's just a ViewProfileNavigationController)
-			
-			let ViewProfileNavigationC = storyboard.instantiateViewController(withIdentifier: "ViewProfileNavigationController")
-            ViewProfileNavigationC.tabBarItem = UITabBarItem(title: "Профиль", image: #imageLiteral(resourceName: "profile"), tag: 3)
-			TabBarC.viewControllers?.append(ViewProfileNavigationC)
-			let user = Account(login: "", password: "", profileType: .view)
-			loadData(acc: user)
-			
-		}
-//        present(TabBarC, animated: true, completion: nil)
-        TabBar = TabBarC
+		self.view.showBlurLoader()
     }
     
     override func viewDidAppear(_ animated: Bool) {
-        present(TabBar, animated: false, completion: nil)
+		DispatchQueue.global(qos: .userInitiated).async {
+			let storyboard = UIStoryboard(name: "Main", bundle: nil)
+			let TabBarC = storyboard.instantiateViewController(withIdentifier: "TabBar") as! UITabBarController
+			
+			// Adding user's token to keychain (if needed)
+			/*
+			let key: String = "userToken"
+			let data = "testtoken".data(using: .utf8)!
+			
+			if Keychain.save(key: key, data: data)  == errSecSuccess {
+			print("tokenAdded")
+			}
+			*/
+			// Now we are checking, if there is some user's token in keychain
+			// if true, we can determine the type of account and then we can present appropriate tabbar
+			
+			let token = Keychain.load(key: "userToken")
+			if token != nil {
+				
+				let ClientProfileNavigationC = storyboard.instantiateViewController(withIdentifier: "ClientNavigationController")
+				TabBarC.viewControllers?.append(ClientProfileNavigationC)
+				let user = Account(login: "client", password: "client", profileType: .client)
+				loadData(acc: user)
+				
+			} else { // else we can present the registration view (now it's just a ViewProfileNavigationController)
+				
+				let ViewProfileNavigationC = storyboard.instantiateViewController(withIdentifier: "ViewProfileNavigationController")
+				ViewProfileNavigationC.tabBarItem = UITabBarItem(title: "Профиль", image: #imageLiteral(resourceName: "profile"), tag: 3)
+				TabBarC.viewControllers?.append(ViewProfileNavigationC)
+				let user = Account(login: "", password: "", profileType: .view)
+				loadData(acc: user)
+				
+			}
+			//        present(TabBarC, animated: true, completion: nil)
+			self.TabBar = TabBarC
+			
+			sleep(5)
+			DispatchQueue.main.async {
+				self.view.removeBlurLoader()
+			}
+			self.present(self.TabBar, animated: false, completion: nil)
+		}
     }
     
     override func didReceiveMemoryWarning() {
