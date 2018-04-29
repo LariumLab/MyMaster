73850c3ad819c2a1df669700aa2c89b2512f2a46
Nadezda Agildina <a_strelina@inbox.ru>
2018-04-29 20:34:39 +0300

    Add request cell's

---
+++ Scheduling/Search/TableCells/AddRequestDescriptionTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AddRequestDescriptionTableViewCell.swift
+//  Scheduling
+//
+//  Created by macbook on 25.04.2018.
+//  Copyright © 2018 Aksiniya. All rights reserved.
+//
+
+import UIKit
+
+class AddRequestDescriptionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ Scheduling/Search/TableCells/AddRequestTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AddRequestTableViewCell.swift
+//  Scheduling
+//
+//  Created by macbook on 25.04.2018.
+//  Copyright © 2018 Aksiniya. All rights reserved.
+//
+
+import UIKit
+
+class AddRequestTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
